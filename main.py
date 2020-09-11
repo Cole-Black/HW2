a65ccb6de7e0c10796bd4bf6a465294c338ac37f
ColeBlack3 <blackstallardc@gmail.com>
2020-09-11 16:07:40 +0000

    the values did not need to be strings, they are straight values

--- main.py
+++ main.py
@@ -3,38 +3,38 @@
 
 def getGradePoint(Gin):
   if Gin == "A":
-    grade = "4.0"
+    grade = 4.0
   elif Gin == "A-":
-    grade = "3.67"
+    grade = 3.67
   elif Gin == "B+":
-    grade = "3.33"
+    grade = 3.33
   elif Gin == "B":
-    grade = "3.0"
+    grade = 3.0
   elif Gin == "B-":
-    grade = "2.67"
+    grade = 2.67
   elif Gin == "C+":
-    grade = "2.33"
+    grade = 2.33
   elif Gin == "C":
-    grade = "2.0"
+    grade = 2.0
   elif Gin == "D":
-    grade = "1.0"
+    grade = 1.0
   else: 
-    grade = "0.0"
+    grade = 0.0
   return grade 
 
 def run():
   mark1  = input("Enter your course 1 letter grade: ")
-  GPA1 = float(getGradePoint(mark1))
+  GPA1 = getGradePoint(mark1)
   C1 = float(input("Enter your course 1 credit: "))
   print(f"Grade point for course 1 is: {GPA1}.")
 
   mark2  = input("Enter your course 2 letter grade: ")
-  GPA2 = float(getGradePoint(mark2))
+  GPA2 = getGradePoint(mark2)
   C2 = float(input("Enter your course 2 credit: "))
   print(f"Grade point for course 2 is: {GPA2}.")
 
   mark3  = input("Enter your course 3 letter grade: ")
-  GPA3 = float(getGradePoint(mark3))
+  GPA3 = getGradePoint(mark3)
   C3 = float(input("Enter your course 3 credit: "))
   print(f"Grade point for course 3 is: {GPA3}.")
 
